a99f401764d8887abc61a3018a957b43503caf6a
rendyep <rendyekoprastiyo@gmail.com>
2014-06-02 13:34:07 +0700

    add licence in each file

--- src/Winata/Service/Session/SessionStorage.php
+++ src/Winata/Service/Session/SessionStorage.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+* Winata (https://github.com/rendyep/winata)
+*
+* @link https://github.com/rendyep/winata for the canonical source repository
+* @copyright Copyright (c) 20014 Winata Technology Inc. (https://github.com/rendyep/winata)
+* @license https://github.com/rendyep/winata/blob/master/LICENCE
+*/
+
 namespace Winata\Service\Session;
 
 class SessionStorage
